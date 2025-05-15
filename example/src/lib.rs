a0ddfaa28c51e879bd1c3717eeaa8c56eff62610
Alejandro Lugo <alu0101329185@ull.edu.es>
2025-05-15 12:44:26 +0100

    feat: init state in contract execute

--- example/src/lib.rs
+++ example/src/lib.rs
@@ -18,8 +18,8 @@ enum StateEvent {
 }
 
 #[unsafe(no_mangle)]
-pub unsafe fn main_function(state_ptr: i32, event_ptr: i32, is_owner: i32) -> u32 {
-  sdk::execute_contract(state_ptr, event_ptr, is_owner, contract_logic)
+pub unsafe fn main_function(state_ptr: i32, init_state_ptr: i32, event_ptr: i32, is_owner: i32) -> u32 {
+  sdk::execute_contract(state_ptr, init_state_ptr, event_ptr, is_owner, contract_logic)
 }
 
 #[unsafe(no_mangle)]
